c293c22cfdd000af9b9a86083d91a64b8d9bc220
Siddhant Jain <codebysidd@gmail.com>
2024-12-09 16:57:39 +0530

    hero resposiveness added

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -8,13 +8,15 @@ const Hero = () => {
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 pt-14">
       <Logo />
       <div className=" flex flex-col gap-2 justify-center items-center">
-        <h1 className=" text-4xl font-bold">Welcome to Peerlist Forms</h1>
-        <p className=" text-lg font-medium">
+        <h1 className=" text-2xl lg:text-4xl font-bold">
+          Welcome to Peerlist Forms
+        </h1>
+        <p className="text-sm text-center lg:text-lg font-medium">
           Create customizable forms with variety of question types easily
         </p>
       </div>
       <Signin />
-      <div className="relative h-[80vh] w-[80vw]  rounded-xl">
+      <div className="relative h-[35vh] w-[100vw] lg:h-[80vh] lg:w-[80vw]  rounded-xl">
         <Image
           src="/heroImage.png"
           alt="hero image"
